d5c7f518d2cac62d08c83590c7bfd83c5a662ac9
Justin Trautmann <justin.trautmann@student.hpi.de>
2019-10-21 21:05:01 +0200

    Step 3: Chunks

--- src/lib/storage/chunk.cpp
+++ src/lib/storage/chunk.cpp
@@ -16,6 +16,7 @@ namespace opossum {
 
 void Chunk::add_segment(std::shared_ptr<BaseSegment> segment) {
   // Implementation goes here
+  _segments.push_back(segment);
 }
 
 void Chunk::append(const std::vector<AllTypeVariant>& values) {
@@ -24,17 +25,21 @@ void Chunk::append(const std::vector<AllTypeVariant>& values) {
 
 std::shared_ptr<BaseSegment> Chunk::get_segment(ColumnID column_id) const {
   // Implementation goes here
-  return nullptr;
+  return _segments[column_id];
 }
 
 uint16_t Chunk::column_count() const {
   // Implementation goes here
-  return 0;
+  return _segments.size();
 }
 
 uint32_t Chunk::size() const {
   // Implementation goes here
-  return 0;
+  if (_segments.size() != 0) {
+  	return _segments[0]->size();
+  } else {
+  	return 0;
+  }
 }
 
 }  // namespace opossum
